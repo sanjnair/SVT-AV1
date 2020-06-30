0ad661e0f1caa48eb08c2f3a6b3b517d93344e01
hguermaz <hassen.guermazi@intel.com>
2020-06-30 06:07:58 -0700

    update_0

--- Source/Lib/Common/Codec/EbDefinitions.h
+++ Source/Lib/Common/Codec/EbDefinitions.h
@@ -598,7 +598,7 @@ extern "C" {
 #define ME_USE_NSC_SETTING         1 // 
 #define FIX_HIGH_MOTION            1
 
-#define OPTIMIZE_SPARSE_SEARCH     1
+#define OPTIMIZE_SPARSE_SEARCH     0
 #define FIX_TOP_N_SEARCH           0
 
 #define TOWARDS_FINAL_0            0
